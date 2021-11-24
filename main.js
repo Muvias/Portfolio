b713204e201363493810c873615ebc3ab428fb44
Vinicius Perazza <vi_guepardo@hotmail.com>
2021-11-24 18:58:14 -0300

    Portfolio

--- main.js
+++ main.js
@@ -5,3 +5,17 @@ function toggleMenu(){
     toggleMenu.classList.toggle('active')
     sidebar.classList.toggle('active')
 }
+
+const backToTopButton = document.querySelector('.back-to-top')
+
+function backToTop(){
+  if(window.scrollY >= 750) {
+    backToTopButton.classList.add('show')
+  } else{
+    backToTopButton.classList.remove('show')
+  }
+}
+
+window.addEventListener('scroll', function(){
+    backToTop()
+  })
